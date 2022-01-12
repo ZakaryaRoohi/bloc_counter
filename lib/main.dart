23317c9d243d129792b7efb91f9b42c39fbf45db
Roohi <roohi@neron.com>
2022-01-12 18:05:54 +0330

    create CounterApp and extends MaterialApp and define home as CounterPage

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:bloc/bloc.dart';
 import 'package:bloc_counter/counter_observer.dart';
 import 'package:flutter/material.dart';
 
+import 'app.dart';
+
 
 void main(){
   BlocOverrides.runZoned(
-      ()=> runApp(const CoounterApp()),
+      ()=> runApp(const CounterApp()),
     blocObserver: CounterObserver(),
 
   );
