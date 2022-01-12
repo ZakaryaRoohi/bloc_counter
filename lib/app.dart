c6507517e407cc5b188cd44d485d81a7c7d62851
Roohi <roohi@neron.com>
2022-01-12 18:14:29 +0330

    implement CounterPage.dart . this class create CounterCubit  and providing it to CounterView

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 
+import 'counter/view/CounterPage.dart';
+
 class CounterApp extends MaterialApp{
 
   const CounterApp({Key? key}) : super(key: key , home: const CounterPage());
---
+++ lib/counter/view/CounterPage.dart
@@ -0,0 +1,21 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterPage extends StatelessWidget{
+
+  const CounterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return BlocProvider(
+        create: (_) => CounterCubit(),
+      child: CounterView(),
+    );
+
+  }
+
+
+}
